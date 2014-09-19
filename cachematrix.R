4602d5b1a8e79491fbc6b17e17b943e22f1fb75f
Abhijith Ramalingam <ajibith1995@gmail.com>
2014-09-19 14:34:17 -0400

    This is the submission file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+	#The makeCacheMatrix returns a list of functions we can use to get and
+	#set the input matrix. It also has appropriately named 
+	#functions to solve for the input's inverse
+	#Input : Accepts a matrix and stores it in x$get
+	#Output : List containing usable functions for getting and setting X
+	
+	makeCacheMatrix <- function(x = matrix()) {
+	# Accepts a matrix and stores it in a variable named x
+	 
+	  inverseX <- NULL 
+	  #Stores the inverse of x. Initializes as NULL
+	  
+	  
+	  # Setter function
+	  set <- function(y) { 
+		   x <<- y
+		   inverseX <<- NULL
+	  }
+	  #Getter function
+	  get <- function() {
+			x
+	  }
+	  # Sets/Calculates the inverse of X
+	 setinverse <- function(m) inverseX <<- m
+	 #Getter function for the inverse of X
+	 getinverse <- function() inverseX
+	 #Returns a list containing all the required functions
+	  list(set = set, 
+		   get = get,
+		   setinverse = setinverse,
+		   getinverse = getinverse
+		   )
+	  
 
-makeCacheMatrix <- function(x = matrix()) {
+	 
+	}
 
-}
+	#The cachesolve function returns the inverse of an input matrix.
+    #It uses the cached matrix if possible
+	#Input :  A matrix
+	#Output : Inverse of given matrix
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	  cachesolve <- function(x, ...) {
+		  inv <- x$getinverse()
+		  if(!is.null(inv)) 
+		  { 
+		    # If the inverse of X already exists then it is NOT null. Thus 
+			# it is in the cached
+			# Thus all we do is take the already existing inverse and
+			# return it back to whoever called it.
+				message ("Getting the cached inverse of X")
+				return (inv)
+		  }
+		   # If the inverse does not already exist then we find the 
+		   # inverse of X using the solve method
+		   #  After inverting the matrix we set inverseX as the 
+		   # inverse of X by calling the setter function.
+		  data <- x$get() 
+		  inv <- solve(x)
+		  x$setinverse(inv)
+		  inv
+	}
+ 
